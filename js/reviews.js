0e9d8dcd1df8eb4e5b3ed680f2d0774ee1df7c87
james-tierney <jamesytier@hotmail.com>
2023-11-26 20:14:23 +0000

    smoke shop

--- js/reviews.js
+++ js/reviews.js
@@ -1,26 +1,41 @@
-const reviews = [
-  {
-    id: 1,
-    site: "Study Zone",
-    rating: 4,
-    review:
-      "James delivered an exceptional application with The Study Zone site. The design is sleek and modern, which is a testament to his expertise. Now, I have all my favorite lofi stations in one place, making study sessions more enjoyable. James' attention to detail and commitment to user satisfaction was very noticeable, and the delivered project showcased such.",
-  },
-  {
-    id: 2,
-    site: "Art Listing",
-    rating: 5,
-    review:
-      "The art listing site is an invaluable tool for artists. It simplifies the process of listing and managing my artwork, and the design is visually appealing. The project was delivered promptly, and I am highly satisfied with the outcome.",
-  },
-  {
-    id: 3,
-    site: "Mobile App Moonrise",
-    rating: 3,
-    review:
-      "Working with James during the development of Moonrise was a great experience. He was professional and transparent throughout the process, providing regular updates and addressing all my requirements. Moonrise perfectly fits my needs for tracking and enhancing my sleep. It was delivered on time, and the design is smooth, intuitive, and fits the mood well. I'm highly satisfied with its performance and its ability to address my sleep-related needs.",
-  },
-];
+// const reviews = [
+//   {
+//     id: 1,
+//     site: "Study Zone",
+//     rating: 4,
+//     review:
+//       "James delivered an exceptional application with The Study Zone site. The design is sleek and modern, which is a testament to his expertise. Now, I have all my favorite lofi stations in one place, making study sessions more enjoyable. James' attention to detail and commitment to user satisfaction was very noticeable, and the delivered project showcased such.",
+//   },
+//   {
+//     id: 2,
+//     site: "Art Listing",
+//     rating: 5,
+//     review:
+//       "The art listing site is an invaluable tool for artists. It simplifies the process of listing and managing my artwork, and the design is visually appealing. The project was delivered promptly, and I am highly satisfied with the outcome.",
+//   },
+//   {
+//     id: 3,
+//     site: "Mobile App Moonrise",
+//     rating: 3,
+//     review:
+//       "Working with James during the development of Moonrise was a great experience. He was professional and transparent throughout the process, providing regular updates and addressing all my requirements. Moonrise perfectly fits my needs for tracking and enhancing my sleep. It was delivered on time, and the design is smooth, intuitive, and fits the mood well. I'm highly satisfied with its performance and its ability to address my sleep-related needs.",
+//   },
+// ];
+let reviews;
+const placeId =
+  "EjExNDE5IEdsZW5uIFN0IFNXIHN1aXQgMjA0LCBEZWNhdHVyLCBBTCAzNTYwMywgVVNBIiQaIgoWChQKEgkLVDB2d4RiiBHX5Nek11qoCBIIc3VpdCAyMDQ";
+const apiKey = "AIzaSyBBeJPYcvMnLdJXma82OUGRgCtaFIP2bmo";
+const apiUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${apiKey}`;
+
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    // Handle the response data (reviews)
+    reviews = data.result.reviews;
+    console.log("Google Reviews = ", reviews);
+    // Now you can proceed to display these reviews on your website.
+  })
+  .catch((error) => console.error("Error fetching reviews:", error));
 
 const reviewsContainer = document.getElementById("reviews");
 
